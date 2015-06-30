31ca1108cf5bfc1058092994b25cb9b211169b7b
Fabian <fabian.ed.vega@gmail.com>
2015-06-29 20:39:10 -0600

    no message

--- Treeseed/src/main/webapp/resources/filters/treeSeedFilters.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var treeSeedAppFilters = angular.module('treeSeed.filters', []);
-/* Filters */
-// need load the moment.js to use this filter. 
-  treeSeedAppFilters.filter('fromNow', function() {
-    return function(date) {
-      return moment(date).fromNow();
-    }
-  });
