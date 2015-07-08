3c39e5602a85e67380b6ebc034d420acac9e9133
Ricardo Bonilla <rbonillas@ucenfotec.ac.cr>
2015-07-07 20:58:22 -0600

    Adding Navigation with the Landing Page, and also adding the filter in the query for the search to only bring NonProfits when they are active

--- Treeseed/src/main/java/com/treeseed/repositories/NonprofitRepository.java
+++ Treeseed/src/main/java/com/treeseed/repositories/NonprofitRepository.java
@@ -16,24 +16,15 @@ public interface NonprofitRepository extends
 	
 	public static final int PAGE_SIZE = 10;
 
-	@Query("SELECT p FROM Nonprofit p WHERE ( :nameNull is null or p.name like :name)")
-	   public Page<Nonprofit> find(@Param("nameNull") String nameNull, @Param("name") String name,
-			   Pageable pageable);
-	
 	
 	@Query("SELECT p FROM Nonprofit p inner join p.cause c inner join p.country d WHERE ( :nameNull is null or p.name like :name) and "
 			+ "( :country = 0 or d.id = :country) and "
-			+ "( :cause = 0 or c.id = :cause)")
+			+ "( :cause = 0 or c.id = :cause) and p.isActive = 1")
 	   public Page<Nonprofit> findConTodo(@Param("nameNull") String nameNull, @Param("name") String name,
 			   @Param("country") int country,
 			   @Param("cause") int cause,
 			   Pageable pageable);
-	
-	
-	@Query("SELECT p FROM Nonprofit p WHERE ( :nameNull is null or p.name like :name) and "
-			+ "( :countryNull is null or p.country like :country) and "
-			+ "( :causeNull is null or p.cause like :cause)")
-	
+
 	Page<Nonprofit> findAll(Pageable pageable);
 	Page<Nonprofit> findByNameContaining(String name,
 			Pageable pageable);
