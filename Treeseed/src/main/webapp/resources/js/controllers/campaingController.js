c496536a2fed848caef5d5034d258685fa644aaa
Fabian <fabian.ed.vega@gmail.com>
2015-07-15 14:52:22 -0600

    completo front-end

--- Treeseed/src/main/webapp/resources/js/controllers/campaingController.js
+++ Treeseed/src/main/webapp/resources/js/controllers/campaingController.js
@@ -8,16 +8,19 @@ treeSeedAppControllers.controller('campaingCreateController', function($http,
 	$scope.stateInput3 = false;
 	$scope.stateInput4 = false;
 	$scope.stateInput5 = false;
-	$scope.stateInput6 = false;
+	$scope.mindate = new Date();
 
 	$scope.campaing = {};
 	$scope.uploadImage = false;
 	$scope.campaing.name = "";
 	$scope.campaing.description = "";
-	$scope.campaing.date = "";
+	$scope.campaing.date = new Date();
 	$scope.campaing.amount = "";
 	$scope.image = "";
 
+	$scope.hola = function() {
+		alert("hola");
+	}
 	$scope.create = function(event) {
 
 		this.onError = false;
@@ -31,127 +34,113 @@ treeSeedAppControllers.controller('campaingCreateController', function($http,
 				amount : $scope.campaing.amount
 			},
 			file : $scope.image,
-		}).success(function(response){
-			   
+		}).success(function(response) {
 
-			   $state.go('treeSeed.campaing', {campaing: response.campaingId});
-		   }) 
-
-		$scope.$on('profilePicture', function(event, args) {
-			$scope.image = args;
-			var file = args;
-			var imageType = /image.*/;
-
-			if (file.type.match(imageType)) {
-				var reader = new FileReader();
-
-				reader.onload = function(e) {
-					var img = new Image();
-					img.src = reader.result;
-					fileDisplayArea.src = img.src;
-				}
-				reader.readAsDataURL(file);
-				
-				$scope.uploadImage =true;
-
-			} else {
-				$scope.uploadImage = false;
-				alert("File not supported!");
-			}
-			$scope.progressControl(6,uploadImage);
-		});
-
-		$scope.progressControl = function(input, valid) {
-
-			switch (input) {
-			case 1:
-				if (valid) {
-					if (!$scope.stateInput1) {
-						$scope.percent = $scope.percent + 20;
-						$scope.stateInput1 = true;
-					}
-
-				} else {
-					if ($scope.stateInput1) {
-						$scope.percent = $scope.percent - 20;
-						$scope.stateInput1 = false;
-					}
-				}
-				break;
-			case 2:
-				if (valid) {
-					if (!$scope.stateInput2) {
-						$scope.percent = $scope.percent + 20;
-						$scope.stateInput2 = true;
-					}
-
-				} else {
-					if ($scope.stateInput2) {
-						$scope.percent = $scope.percent - 20;
-						$scope.stateInput2 = false;
-					}
-				}
-				break;
-			case 3:
-				if (valid) {
-					if (!$scope.stateInput3) {
-						$scope.percent = $scope.percent + 20;
-						$scope.stateInput3 = true;
-					}
-
-				} else {
-					if ($scope.stateInput3) {
-						$scope.percent = $scope.percent - 20;
-						$scope.stateInput3 = false;
-					}
-				}
-				break;
-			case 4:
-				if (valid) {
-					if (!$scope.stateInput4) {
-						$scope.percent = $scope.percent + 20;
-						$scope.stateInput4 = true;
-					}
-
-				} else {
-					if ($scope.stateInput4) {
-						$scope.percent = $scope.percent - 20;
-						$scope.stateInput4 = false;
-					}
-				}
-				break;
-			case 5:
-				if (valid) {
-					if (!$scope.stateInput5) {
-						$scope.percent = $scope.percent + 20;
-						$scope.stateInput5 = true;
-					}
-
-				} else {
-					if ($scope.stateInput5) {
-						$scope.percent = $scope.percent - 20;
-						$scope.stateInput5 = false;
-					}
-				}
-				break;
-				
-			case 6:
-				if (valid) {
-					if (!$scope.stateInput6) {
-						$scope.percent = $scope.percent + 20;
-						$scope.stateInput6 = true;
-					}
-
-				} else {
-					if ($scope.stateInput6) {
-						$scope.percent = $scope.percent - 20;
-						$scope.stateInput6 = false;
-					}
-				}
-				break;
-			}
-		}
+			$state.go('treeSeed.campaing', {
+				campaing : response.campaingId
+			});
+		})
 
 	};
+
+	$scope.$on('profilePicture', function(event, args) {
+		$scope.image = args;
+		var file = args;
+		var imageType = /image.*/;
+
+		if (file.type.match(imageType)) {
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+				var img = new Image();
+				img.src = reader.result;
+				fileDisplayArea.src = img.src;
+			}
+			reader.readAsDataURL(file);
+
+			$scope.uploadImage = true;
+
+		} else {
+			$scope.uploadImage = false;
+			alert("File not supported!");
+		}
+		$scope.progressControl(5, $scope.uploadImage);
+	});
+
+	$scope.progressControl = function(input, valid) {
+
+		switch (input) {
+		case 1:
+			if (valid) {
+				if (!$scope.stateInput1) {
+					$scope.percent = $scope.percent + 20;
+					$scope.stateInput1 = true;
+				}
+
+			} else {
+				if ($scope.stateInput1) {
+					$scope.percent = $scope.percent - 20;
+					$scope.stateInput1 = false;
+				}
+			}
+			break;
+		case 2:
+			if (valid) {
+				if (!$scope.stateInput2) {
+					$scope.percent = $scope.percent + 20;
+					$scope.stateInput2 = true;
+				}
+
+			} else {
+				if ($scope.stateInput2) {
+					$scope.percent = $scope.percent - 20;
+					$scope.stateInput2 = false;
+				}
+			}
+			break;
+		case 3:
+			if (valid) {
+				if (!$scope.stateInput3) {
+					$scope.percent = $scope.percent + 20;
+					$scope.stateInput3 = true;
+				}
+
+			} else {
+				if ($scope.stateInput3) {
+					$scope.percent = $scope.percent - 20;
+					$scope.stateInput3 = false;
+				}
+			}
+			break;
+		case 4:
+			if (valid) {
+				if (!$scope.stateInput4) {
+					$scope.percent = $scope.percent + 20;
+					$scope.stateInput4 = true;
+				}
+
+			} else {
+				if ($scope.stateInput4) {
+					$scope.percent = $scope.percent - 20;
+					$scope.stateInput4 = false;
+				}
+			}
+			break;
+		case 5:
+			if (valid) {
+				if (!$scope.stateInput5) {
+					$scope.percent = $scope.percent + 20;
+					$scope.stateInput5 = true;
+				}
+
+			} else {
+				if ($scope.stateInput5) {
+					$scope.percent = $scope.percent - 20;
+					$scope.stateInput5 = false;
+				}
+			}
+			break;
+		}
+	}
 
 });
