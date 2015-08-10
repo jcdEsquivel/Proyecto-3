1609e1d4aafad7313a41d947c161910a2246fd9e
Camilo Aguero <camiloab01@gmail.com>
2015-08-09 21:09:48 -0500

    UI done

--- Treeseed/src/main/webapp/resources/js/controllers/donorController.js
+++ Treeseed/src/main/webapp/resources/js/controllers/donorController.js
@@ -645,60 +645,41 @@ treeSeedAppControllers.controller('donorSettingsController', function($scope,
 treeSeedAppControllers.controller('donorPortfolioController', function($scope,
 		$http, $location, $modal, $log, $timeout, $stateParams, Session, $state, $rootScope, $sharedData, AUTH_EVENTS, AuthService) {
 
-	$scope.d = [ [1,6.5],[2,6.5],[3,7],[4,8],[5,7.5],[6,7],[7,6.8],[8,7],[9,7.2],[10,7],[11,6.8],[12,7] ];
+	$scope.requestObject = {};
+	$scope.requestObject.donorId = $stateParams.donorId;
+	$scope.causes;
 
-    $scope.d0_1 = [ [0,7],[1,6.5],[2,12.5],[3,7],[4,9],[5,6],[6,11],[7,6.5],[8,8],[9,7] ];
+	$scope.getRecurrableData = function() {
+		$http.post('rest/protected/recurrableInformation/getRecurrableInformation', 
+			$scope.requestObject).then(function(response) {
+				if(response.data.code=="200"){
+					console.log("COOL");
+					$scope.d3 = response.data.results;
+					setData();
+				}
+				else if(response.data.code=="400"){
+					console.log("ERROR");
+				}
+		});
+	};
 
-    $scope.d0_2 = [ [0,4],[1,4.5],[2,7],[3,4.5],[4,3],[5,3.5],[6,6],[7,3],[8,4],[9,3] ];
+	$scope.getRecurrableData();
 
-    $scope.d1_1 = [ [10, 120], [20, 70], [30, 70], [40, 60] ];
+	function setData(){
+		for (var i = $scope.d3.length - 1; i >= 0; i--) {
+			$scope.d3[i].data = $scope.d3[i].amount;
+			if($scope.selectLang == 'English'){
+				$scope.d3[i].label = $scope.d3[i].englishCause;
+			}else{
+				$scope.d3[i].label = $scope.d3[i].spanishCause;
+			}
+		};
+	}
 
-    $scope.d1_2 = [ [10, 50],  [20, 60], [30, 90],  [40, 35] ];
-
-    $scope.d1_3 = [ [10, 80],  [20, 40], [30, 30],  [40, 20] ];
-
-    $scope.d2 = [];
-
-    for (var i = 0; i < 20; ++i) {
-      $scope.d2.push([i, Math.round( Math.sin(i)*100)/100] );
-    }   
-
-    $scope.d3 = [ 
-      { label: "Water", data: 40 }, 
-      { label: "Anmals", data: 10 },
-      { label: "Children", data: 20 },
-      { label: "Education", data: 12 },
-      { label: "Enviroment", data: 18 }
-    ];
-
-    $scope.refreshData = function(){
-      $scope.d0_1 = $scope.d0_2;
-    };
-
-    $scope.getRandomData = function() {
-      var data = [],
-      totalPoints = 150;
-      if (data.length > 0)
-        data = data.slice(1);
-      while (data.length < totalPoints) {
-        var prev = data.length > 0 ? data[data.length - 1] : 50,
-          y = prev + Math.random() * 10 - 5;
-        if (y < 0) {
-          y = 0;
-        } else if (y > 100) {
-          y = 100;
-        }
-        data.push(Math.round(y*100)/100);
-      }
-      // Zip the generated y values with the x values
-      var res = [];
-      for (var i = 0; i < data.length; ++i) {
-        res.push([i, data[i]])
-      }
-      return res;
-    }
-
-    $scope.d4 = $scope.getRandomData();
+    /*$scope.d3 = [ 
+      { label: "Water", data: 400 }, 
+      { label: "Anmals", data: 5000 }
+    ];*/
 });
 
 
