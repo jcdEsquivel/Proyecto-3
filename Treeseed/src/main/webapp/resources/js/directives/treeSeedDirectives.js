dd7f5a18091e65d8c184ba5da19f11203fe68a9d
camiloab01 <camiloab01@gmail.com>
2015-08-24 18:09:37 -0600

    Fix de formato de constraseña

--- Treeseed/src/main/webapp/resources/js/directives/treeSeedDirectives.js
+++ Treeseed/src/main/webapp/resources/js/directives/treeSeedDirectives.js
@@ -559,6 +559,30 @@ treeSeedAppDirectives.directive('loginDialog', function(AUTH_EVENTS) {
       });
     }
   };
+})
+
+
+.directive('passwordFormat', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+
+                scope.pwdValidLength = (viewValue && viewValue.length >= 8 ? 'valid' : undefined);
+                scope.pwdHasLetter = (viewValue && /[A-z]/.test(viewValue)) ? 'valid' : undefined;
+                scope.pwdHasNumber = (viewValue && /\d/.test(viewValue)) ? 'valid' : undefined;
+
+                if(scope.pwdValidLength && scope.pwdHasLetter && scope.pwdHasNumber) {
+                    ctrl.$setValidity('pwd', true);
+                    return viewValue;
+                } else {
+                    ctrl.$setValidity('pwd', false);                    
+                    return undefined;
+                }
+
+            });
+        }
+    };
 });
 
 
